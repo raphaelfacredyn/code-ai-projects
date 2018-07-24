ba4fccbbda6505b9b23320dcf78493dced03d381
arthur109 <arthurfaj@gmail.com>
2018-07-24 08:02:31 -0700

    Finshed Recursive

--- Day1/3-It_Moves!/sketch.js
+++ Day1/3-It_Moves!/sketch.js
@@ -1,13 +1,13 @@
-var ballXposition = 0
-
-function setup() {
-    createCanvas(500, 500)
-}
-
-function draw() {
-    ballXposition = ballXposition + 3
-    ellipse(ballXposition, 250, 20, 20)
-}
+// var ballXposition = 0
+//
+// function setup() {
+//     createCanvas(500, 500)
+// }
+//
+// function draw() {
+//     ballXposition = ballXposition + 3
+//     ellipse(ballXposition, 250, 20, 20)
+// }
 ////-----------------------------------------
 // //Challenge (Up, Down)
 // var ballYposition = 0
@@ -40,21 +40,21 @@ function draw() {
 // }
 ////-----------------------------------------
 // //Challenge (random)
-// var ballYposition
-// var ballXposition
-//
-// function setup() {
-//     createCanvas(500, 500)
-//     background(255, 157, 72)
-//     ballXposition = width/2
-//     ballYposition = height/2
-// }
-//
-// function draw() {
-//
-//     fill(255, 255, 255)
-//     ballYposition = ballYposition + random(-5,5)
-//     ballXposition = ballXposition + random(-5,5)
-//     ellipse(ballXposition, ballYposition, 20, 20)
-// }
+var ballYposition
+var ballXposition
+
+function setup() {
+    createCanvas(500, 500)
+    background(255, 157, 72)
+    ballXposition = width/2
+    ballYposition = height/2
+}
+
+function draw() {
+
+    fill(255, 255, 255)
+    ballYposition = ballYposition + random(-5,5)
+    ballXposition = ballXposition + random(-5,5)
+    ellipse(ballXposition, ballYposition, 20, 20)
+}
 ////-----------------------------------------
--- Day2/7-Forever...Oops/README.md
+++ Day2/7-Forever...Oops/README.md
@@ -1,1 +1,75 @@
-# Forever... Oops - Notes
+#### Forever... Oops - Notes
+
+its, a function
+
+that calls itself
+
+we can create a function called diagonal rectangle
+
+```js
+function diagonalRectangle(){
+    // Code in here
+}
+```
+
+this function will draw rectangle given the position and a size
+
+```js
+function diagonalRectangle(xpos,ypos,width,height){
+   rect(xpos,ypos,width,height)
+}
+```
+
+so basically, we did nothing, we just but a "wrapper" around the rectangle function
+
+but what if we do this?
+
+```js
+function diagonalRectangle(xpos,ypos,width,height){
+   rect(xpos,ypos,width,height)
+   diagonalRectangle(xpos,ypos,width,height)
+}
+```
+
+the function runs itself, which runs itself...
+
+but this will just draw an infinite number of rectangle, kind of useless
+
+so... what if I do this
+
+```js
+function diagonalRectangle(xpos,ypos,width,height){
+   rect(xpos,ypos,width,height)
+   diagonalRectangle(xpos+20,ypos+20,width,height)
+}
+```
+
+Ok, whats going to happen?
+
+no, lets see who's right
+
+no one, it doesn't work
+
+this is because It goes on forever
+
+so we need to put a limit
+
+```js
+var recursionDepth = 10
+function diagonalRectangle(xpos,ypos,width,height,counter){
+   rect(xpos,ypos,width,height)
+   if(counter < recursionDepth){
+   	diagonalRectangle(xpos+20,ypos+20,width,heightcounter+1)
+   }
+}
+```
+
+now? yep
+
+
+
+Challenge
+
+make this:
+
+(show square fractal)
